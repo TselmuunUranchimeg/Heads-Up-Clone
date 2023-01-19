ede448789d1f7ed85155c5caa6d236923a321a2b
Tselmuun Uranchimeg <tselmuunu@gmail.com>
2023-01-19 21:44:36 +0800

    Added server URL

--- src/Extras/axios.ts
+++ src/Extras/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosIns = axios.create({
-    baseURL: "http://localhost:8000",
+    baseURL: "https://heads-up-clone-server.onrender.com",
     withCredentials: true,
 });
 
